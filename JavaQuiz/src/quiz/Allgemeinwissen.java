93ed76b88c2e1c86c60104669aa8e6afbf89c5bf
MarianWillms-7 <willmsmarian@student.dv-schulen.de>
2026-01-08 09:28:03 +0100

    allgemeinwissen api commit

--- JavaQuiz/src/quiz/Allgemeinwissen.java
+++ JavaQuiz/src/quiz/Allgemeinwissen.java
@@ -1,43 +1,27 @@
-package quiz;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 public class Allgemeinwissen {
-	public static String Fragen(String frage) {
-		String URL = "https://opentdb.com/api.php?amount=50&category=9&type=multiple";
-		String content = URL(URL);
-		return content;
-	}
-
-	public static String URL(String url) {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		HttpGet httpget = new HttpGet(url);
-		String urlContent = "";
-
-		ResponseHandler<String> rh = new ResponseHandler<String>() {
-			@Override
-			public String handleResponse(HttpResponse hr) throws ClientProtocolExeption, IOException {
-				HttpEntity entity = hr.getEntity();
-				if (entity == null) {
-					throw new ClientProtocolException("Response contains no content");
-				}
-				BufferedReader br = new BufferedReader(new InputStreamReader(entity.getContent()));
-
-				StringBuilder sb = new StringBuilder();
-				String output;
-				while ((output = br.readLine()) != null) {
-					sb.append(output);
-				}
-				return sb.toString();
-			}
-		};
+	public static String getFragen() {
 		try {
-			urlContent = httpclient.execute(httpget, rh);
-		} catch (IOException e) {
+			URL url = URI.create("https://opentdb.com/api.php?amount=50&category=9&type=multiple").toURL();
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String inputLine;
+			StringBuilder content = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				content.append(inputLine);
+			}
+			in.close();
+			return content.toString();
+		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		return urlContent;
 	}
 }
