02080ca40c984351cbf529c255a0b17140152f40
MarianWillms-7 <willmsmarian@student.dv-schulen.de>
2026-01-14 17:14:45 +0100

    api wird richtig aufgerufen
    Man kann jetzt zwischen 3 apis entscheiden in einer Datei und diese wird dann durch das thema automatisch ausgewählt...

---
+++ JavaQuiz/src/quiz/QuizDatenQuelle.java
@@ -0,0 +1,41 @@
+package quiz;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class QuizDatenQuelle
+{
+
+	public static String getFragen(String thema)
+	{
+
+		String urlString = "";
+
+		// auswählen
+		if (thema.equalsIgnoreCase("videogames")) {
+			urlString = "https://opentdb.com/api.php?amount=50&category=15&type=multiple";
+		} else if (thema.equalsIgnoreCase("filme")) {
+			urlString = "https://opentdb.com/api.php?amount=50&category=11&type=multiple";
+		} else if (thema.equalsIgnoreCase("allgemeinwissen")) {
+			urlString = "https://opentdb.com/api.php?amount=50&category=9&type=multiple";
+		}
+
+		// Client erstellen
+		HttpClient client = HttpClient.newHttpClient();
+
+		// Anfrage machen
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+
+		try {
+			// Daten holen
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			return response.body();
+
+		} catch (Exception e) {
+			System.out.println("Fehler beim Laden von " + thema + ": " + e.getMessage());
+			return null;
+		}
+	}
+}
