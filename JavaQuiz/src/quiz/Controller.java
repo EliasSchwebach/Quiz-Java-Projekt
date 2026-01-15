ce469a5837ce2f682f8d4187db628616470da3c1
MarianWillms-7 <willmsmarian@student.dv-schulen.de>
2026-01-15 08:18:15 +0100

    api anpassung

--- JavaQuiz/src/quiz/Controller.java
+++ JavaQuiz/src/quiz/Controller.java
@@ -1,27 +1,83 @@
 package quiz;
 
+import java.util.ArrayList;
+
+import java.util.Collections;
+
 public class Controller
+
 {
+
 	private Gui gui;
+
 	private String fragenJson;
 
+	private String[] alleFragenRoh;
+
+	private String aktuelleRichtigeAntwort;
+
 	public Controller()
+
 	{
+
 		this.gui = new Gui(this);
+
 	}
 
 	public void loadAllgemeinwissenFragen()
+
 	{
+
 		fragenJson = QuizDatenQuelle.getFragen("allgemeinwissen");
+
 	}
 
 	public void loadVideoGamesFragen()
+
 	{
+
 		fragenJson = QuizDatenQuelle.getFragen("videogames");
+
 	}
 
 	public void loadMovieFragen()
+
 	{
+
 		fragenJson = QuizDatenQuelle.getFragen("filme");
+
+	}
+
+	public void zeigeAllgemeinwissen()
+
+	{
+
+		AllgemeinwissenPanel meinPanel = new AllgemeinwissenPanel();
+
+
+		this.gui.setView(meinPanel);
+
+	}
+
+	public void zeigeVideoGames()
+
+	{
+
+		VideoGamesPanel meinPanel = new VideoGamesPanel();
+
+
+		this.gui.setView(meinPanel);
+
+	}
+
+	public void zeigeMovie()
+
+	{
+
+		MoviePanel meinPanel = new MoviePanel();
+
+
+		this.gui.setView(meinPanel);
+
 	}
 }
