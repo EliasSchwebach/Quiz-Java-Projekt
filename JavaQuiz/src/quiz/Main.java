d46261d92ba04f47c3a14f6c744b8de9428efa54
EliasSchwebach <eliasschwebach@icloud.com>
2026-01-15 08:09:44 +0100

    test

--- JavaQuiz/src/quiz/Main.java
+++ JavaQuiz/src/quiz/Main.java
@@ -3,7 +3,6 @@ package quiz;
 public class Main {
 
 	public static void main(String[] args) {
-		// macht neuen Windows Look
 		try {
 			javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
